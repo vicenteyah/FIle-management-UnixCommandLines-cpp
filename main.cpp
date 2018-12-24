e0b39dd75cb765033d3547ff45eb0a492eb7e9ef
vicente yah <vicente.yah.dzul@icloud.com>
2018-12-24 11:29:39 -0600

    New function (select) was added

--- main.cpp
+++ main.cpp
@@ -7,18 +7,7 @@ int main(int argc, char const *argv[])
     /* code */
     file init;int op = init.menu();
     while(op!=11){
-        switch(op){
-            case 1: init.option1(); break;
-            case 2: init.option2(); break;
-            case 3: init.option3(); break;
-            case 4: init.option4(); break;
-            case 5: init.option5(); break;
-            case 6: init.option6(); break;
-            case 7: init.option7(); break;
-            case 8: init.option8(); break;
-            case 9: init.option9(); break;
-            case 10: init.option10(); break;
-        }
+        init.select(op);
         op = init.menu();
     }
     system("rm -r DirA");
